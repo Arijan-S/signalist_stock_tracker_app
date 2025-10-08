0daf96fe60b5fa579e4813e7859adb3c6bad04e3
Arijan Sulejmani <arijansulejmani@Arijans-MacBook-Pro.local>
2025-10-08 18:21:44 +0200

    implement daily news summary

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
-import { sendSignUpEmail } from "@/lib/inngest/functions";
+import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 import { inngest } from "@/lib/inngest/inngest";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendSignUpEmail],
+  functions: [sendSignUpEmail, sendDailyNewsSummary],
 });
